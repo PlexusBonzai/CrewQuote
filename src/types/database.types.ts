cb0a7d8118dcaaa11b53f17def06a5d218c4529a
Deon Bruning <deon@plexusmedia.co.za>
2026-07-27 14:36:29 +0200

    Sync database types with hosted Supabase schema

--- src/types/database.types.ts
+++ src/types/database.types.ts
@@ -7,6 +7,11 @@ export type Json =
   | Json[]
 
 export type Database = {
+  // Allows to automatically instantiate createClient with right options
+  // instead of createClient<Database, { PostgrestVersion: 'XX' }>(URL, KEY)
+  __InternalSupabase: {
+    PostgrestVersion: "14.5"
+  }
   public: {
     Tables: {
       business_settings: {
@@ -1300,4 +1305,3 @@ export const Constants = {
     },
   },
 } as const
-
